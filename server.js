6afe768cc5a3211c75a02050aa624bb385cf67dc
Sidharth <sidharthpunathil714@gmail.com>
2021-02-04 01:59:12 +0530

    production config setup

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const server = require("http").Server(app);
 const io = require("socket.io")(server);
 const { v4: uuidv4 } = require('uuid');
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.set("view-engine", "ejs");
 app.use("/room/:id", express.static("public"));
@@ -68,5 +68,6 @@ io.on("connection", socket => {
 });
 
 })
+
 
 server.listen(port);
